eef90e8a0c607e96020fa1f70fed74a3fd7fcb2a
balaxon <balahanov2209@gmail.com>
2022-05-20 16:45:32 +0300

    feat: added copy-directory task(Fri 20.05.2022 16:45)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
-
+const path = require('path');
+const fs = require('fs');
+const mk = require('fs/promises');
+const from = path.join(__dirname+'\\files');
+const to = path.join(__dirname+'\\files-copy');
+fs.stat(to, function(err) {
+    if (!err) {
+        /* console.log('Директория есть'); */
+        copy();
+    }
+    else if (err.code === 'ENOENT') {
+        /* console.log('директории нет'); */
+        mk.mkdir(to);
+        copy();
+    }
+});
+function copy(){
+    fs.readdir(from, (err, files) => {
+        if (err)
+          console.log(err);
+        else {
+          files.forEach(file => {
+              fs.copyFile(from+'\\'+file, to+'\\'+file, (err) => {
+                if (err) throw err;
+              });
+          })
+          console.log('Готово!');
+        }
+      })
+}
