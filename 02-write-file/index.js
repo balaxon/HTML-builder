f8d873be218abb522a1352fdd013d63ae018c82f
balaxon <balahanov2209@gmail.com>
2022-05-20 14:15:49 +0300

    feat: added write-file task(Fri 20.05.2022 14:15)

---
+++ 02-write-file/index.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const fs = require('fs');
+const process = require('process');
+const { stdout, stdin, exit } = process;
+const file = fs.createWriteStream(path.join(__dirname,'text.txt'));
+stdout.write('Введите ваше сообщение:\n');
+stdin.on('data', data => {
+    if(data.toString().trim() === 'exit'){process.exit();}else{file.write(data);}
+});
+process.on('exit', () => stdout.write('До свидания!'));
